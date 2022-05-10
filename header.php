aa164b26983112254e7239e41062916feab7f5eb
accrane <austincrane@gmail.com>
2022-05-10 10:28:15 -0400

    add hours

--- header.php
+++ header.php
@@ -82,10 +82,10 @@ if($postHeroImage) {
 			$trail_text = ($trail_status_option=='open') ? 'Gym Open':'Gym Closed';
 			?>
 			<div class="topinfo">
-				<span class="trail-status el <?php echo $trail_status ?>">
-					<span class="t"><?php echo $trail_text ?></span>
-					<span class="s"></span>
-				</span>
+				<!-- <span class="trail-status el <?php echo $trail_status ?>"> -->
+					<!-- <span class="t"><?php echo $trail_text ?></span>
+					<span class="s"></span> -->
+				<!-- </span> -->
 				<span id="todayLink" class="today el">
 					<?php $today_options = get_field("today","option"); ?>
 					<?php if ($today_options) { ?>
